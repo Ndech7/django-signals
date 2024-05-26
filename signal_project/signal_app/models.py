6b56d6ac19d61739924b9a7cdc1d3d3f9528cce6
Ndech7 <stephennderitu3@gmail>
2024-05-26 09:44:36 +0300

    Define the Profile Model

--- signal_project/signal_app/models.py
+++ signal_project/signal_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    first_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
+    phone_number = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user)
+        
