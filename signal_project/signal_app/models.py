4862ebfc9e3652fadb6ee90ce17216533ce41633
Ndech7 <stephennderitu3@gmail>
2024-05-26 10:00:30 +0300

    Implement Django Signals

--- signal_project/signal_app/models.py
+++ signal_project/signal_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,4 +12,21 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
-        
+    
+    def create_profile(sender,instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+            print('****************')
+            print('Profile Created!')
+            print('****************')
+
+    post_save.connect(create_profile, sender=User)
+
+    def update_profile(sender,instance, created, **kwargs):
+        if created is False:
+            instance.profile.save()
+            print('****************')
+            print('Profile Updated!')
+            print('****************')   
+
+    post_save.connect(update_profile, sender=User)    
